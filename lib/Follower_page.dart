0bb5556bfa1c4266db9b37b2308ee516e87d5a8b
solutionflutter <solutionflutter@gmail.com>
2021-10-21 11:06:07 +0600

    change in followerpage and add the igrone file and follower_modle

--- lib/Follower_page.dart
+++ lib/Follower_page.dart
@@ -1,6 +1,20 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:suntusthsbatchfirst/home_page.dart';
+import 'package:suntusthsbatchfirst/models/follower_modle.dart';
+
+List<Follower> followers =[
+  Follower(username: "Kazi Afroz",friends: 345,image: "images/Posts/Post_1.jpg"),
+  Follower(username: "Homiun Kabir",friends: 456,image: "images/Posts/Post_2.jpg"),
+  Follower(username: "Murshadul Haq",friends: 1098,image: "images/Posts/Post_3.jpg"),
+  Follower(username: "Abir Hassain",friends: 456,image: "images/Posts/Post_4.jpg"),
+  Follower(username: "Murshadul Hossain Babla",friends: 454,image: "images/Posts/Post_5.jpg"),
+  Follower(username: "Kabir Ali",friends: 3232,image: "images/Posts/Post_6.jpg"),
+  Follower(username: "Ali Ahammed Jobaier",friends: 67,image: "images/Posts/Post_7.jpg"),
+  Follower(username: "Ali Hassan Mashror",friends: 54,image: "images/Posts/Post_8.jpg"),
+  Follower(username: "Masrafie bin Mortaza",friends: 67,image: "images/Posts/Post_9.jpg"),
+  Follower(username: "Medhi Hasan",friends: 454,image: "images/Posts/Post_10.jpg"),
+];
 
 class FollowerPage extends StatefulWidget {
   const FollowerPage({Key key}) : super(key: key);
@@ -57,11 +71,20 @@ class _FollowerPageState extends State<FollowerPage> {
           ),
         ],
       ),
-      body: Center(
-        child: Text(
-          "Follower Page"
+      body: Align(
+        alignment: Alignment.center,
+        child: Container(
+          child: ListView.builder(
+              padding: EdgeInsets.symmetric(horizontal: 15),
+              itemCount: followers.length,
+              itemBuilder: (BuildContext context,int index){
+                return Container(
+
+                );
+              }
+          ),
         ),
-      ),
+      )
     );
   }
 }
---
+++ lib/models/follower_modle.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class Follower{
+
+  Follower({
+    @required this.friends =0,
+    @required this.image= "",
+    @required this.username = "",
+ });
+
+  int friends = 0;
+  String image = "";
+  String username = "";
+ }
