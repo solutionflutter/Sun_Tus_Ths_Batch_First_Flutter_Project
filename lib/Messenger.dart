e526f873ed95a64f8e43fa1729d21f14fd938eb2
solutionflutter <solutionflutter@gmail.com>
2021-11-07 10:00:50 +0600

    4_11_2021 source code have pushed

--- lib/Messenger.dart
+++ lib/Messenger.dart
@@ -2,6 +2,19 @@ import 'package:flutter/material.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:suntusthsbatchfirst/home_page.dart';
 
+List<String> images = [
+  "images/profile_images/user_1.jpg",
+  "images/profile_images/user_2.jpg",
+  "images/profile_images/user_3.jpg",
+  "images/profile_images/user_4.jpg",
+  "images/profile_images/user_5.jpg",
+  "images/profile_images/user_6.jpg",
+  "images/profile_images/user_7.jpg",
+  "images/profile_images/user_8.jpg",
+  "images/profile_images/user_9.jpg",
+  "images/profile_images/user_10.jpg"
+];
+
 class MessengerPage extends StatefulWidget {
   const MessengerPage({Key key}) : super(key: key);
 
@@ -54,6 +67,49 @@ class _MessengerPageState extends State<MessengerPage> {
                 ),
               );
             },
+          ),
+        ],
+      ),
+      body: CustomScrollView(
+        scrollDirection: Axis.vertical,
+        slivers: [
+          SliverToBoxAdapter(
+            child: Container(
+              margin: EdgeInsets.symmetric(horizontal: 15, vertical: 15),
+              height: MediaQuery.of(context).size.height / 15,
+              width: MediaQuery.of(context).size.width,
+              child: Text(
+                "Friends",
+                style: TextStyle(
+                  fontSize: 25,
+                  fontWeight: FontWeight.w700,
+                  color: Color.fromRGBO(0, 0, 0, .6),
+                ),
+              ),
+            ),
+          ),
+          SliverList(
+            delegate: SliverChildBuilderDelegate(
+              (BuildContext context, int index) {
+                return Container(
+                  height: MediaQuery.of(context).size.height / 5,
+                  width: MediaQuery.of(context).size.width / 5,
+                  decoration: BoxDecoration(
+                    image: DecorationImage(
+                      image: AssetImage("${images[index]}"),
+                      fit: BoxFit.scaleDown,
+                    ),
+                    border: Border.all(
+                      style: BorderStyle.solid,
+                      width: 3,
+                      color: Color.fromRGBO(0, 0, 0, .6),
+                    ),
+                    shape: BoxShape.circle,
+                  ),
+                );
+              },
+              childCount: images.length,
+            ),
           ),
         ],
       ),
